6949787337b3cc2835a38b424d0803f589f66937
PushpakChaudhari77 <pushpakc357@gmail.com>
2022-08-07 10:56:24 +0530

    delete unwanted fies

--- About.java
+++
@@ -1,8 +0,0 @@
-class{
-public static void main(String[] args){
-System.out.println("Hello World");
-System.out.println("Java....");
-System.out.println("Java Thinks Book");
-  System.out.println("----------");
-}
-}
