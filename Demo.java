456f309a0e43fc6ae336a3b515e032dd19135b84
PushpakChaudhari77 <pushpakc357@gmail.com>
2022-08-07 14:32:24 +0530

    123 bug fix

--- Demo.java
+++ Demo.java
@@ -3,6 +3,11 @@ public static void main(String [] args){
 System.out.println("Java");
 System.out.println("Hello world");
 System.out.println("Hello Wardha");
+//bug 123 fix
+
+System.out.println("Hello world.........");
+System.out.println("Hello Prince");
+
 
 }
 }
